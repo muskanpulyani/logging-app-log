bcb4ff9960fa6dc34e6305bdb5f0a0d1f51335f5
muskanpulyani <muskan.pulyani@myparkplus.com>
2021-09-18 12:26:03 +0530

    added

--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostbyname("13.127.122.142"), 1241))
+    s.connect((socket.gethostbyname(sys.argv[1]), 1241))
     while True:
         full_msg = ''
         new_msg = True
